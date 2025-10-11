5192c31956ff8a80df29b5c20c0cb0bba01855e5
juancastiblancoUNP <juan.castiblanco@unp.gov.co>
2025-10-11 11:35:47 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('CLIENT_ID', '01cd23bb-037e-49d4-85f4-780c6bd0e335'); 
+define('CLIENT_SECRET', 'e7e733bd-920c-47a6-8133-c1e42b13d19b'); 
+define('REDIRECT_URI', 'https://lineavida103-cuhjcyfkfagthhf3.canadacentral-01.azurewebsites.net/.auth/login/aad/callback'); 
+define('TENANT_ID', '58ec5e61-0ed7-4021-a4f8-c2e2b3b9f5ff'); 
+define('GRAPH_SCOPES', 'openid profile User.Read');
+?>
