53c6b68efb8aa625d4b96e83ebdaa137ee62d327
juancastiblancoUNP <juan.castiblanco@unp.gov.co>
2025-10-12 18:31:49 -0500

    Update perfil.php

--- perfil.php
+++ perfil.php
@@ -5,7 +5,7 @@ $email = $_SERVER['HTTP_X_MS_CLIENT_PRINCIPAL_NAME'];
 
 // 2. Verificar si la variable existe y si hay un usuario autenticado
 if (isset($email)) {
-    echo "¡Bienvenido! Su correo electrónico es: **" . htmlspecialchars($email) . "**";
+    echo htmlspecialchars($email);
 } else {
     // Esto podría ocurrir si la página se accede sin pasar por el flujo de autenticación de Easy Auth,
     // o si el modo de autenticación está configurado como "Permitir solicitudes anónimas".
