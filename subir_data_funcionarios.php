54bef53e5b24ddd16dd142fc37e5ba8c39cbfff0
juancastiblancoUNP <juan.castiblanco@unp.gov.co>
2025-10-13 16:22:58 -0500

    Update subir_data_funcionarios.php

--- subir_data_funcionarios.php
+++ subir_data_funcionarios.php
@@ -10,7 +10,7 @@ $azureUsername = "juan.castiblanco@unp.gov.co"; // Debe ser su usuario AAD
 $azurePassword = "Unp2025*";        // Debe ser la contraseña de su usuario AAD
 
 // 3. DEFINICIÓN DEL DSN (Incluyendo la opción clave de AAD)
-$dsn = "sqlsrv:server=$serverName; Database=$databaseName;Authentication=ActiveDirectoryPassword";
+$dsn = "sqlsrv:server=$serverName; Database=$databaseName;LoginTimeout=180;Authentication=ActiveDirectoryPassword";
 
 try {
     // 4. Conexión usando PDO
@@ -18,8 +18,7 @@ try {
         $dsn,
         $azureUsername, 
         $azurePassword,
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_TIMEOUT => 180]
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
     );
     
 
