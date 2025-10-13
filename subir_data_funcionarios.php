d888563e1356c203abdc6a38ad9914c9c51d7990
juancastiblancoUNP <juan.castiblanco@unp.gov.co>
2025-10-13 15:55:56 -0500

    Update subir_data_funcionarios.php

--- subir_data_funcionarios.php
+++ subir_data_funcionarios.php
@@ -18,8 +18,7 @@ try {
         $dsn,
         $azureUsername, 
         $azurePassword,
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_TIMEOUT => 60]
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
     );
     
 
